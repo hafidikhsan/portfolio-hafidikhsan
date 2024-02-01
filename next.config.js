ec99450c5bdf1141bfc3e9d4b1178469f50d34c6
hafidikhsan <harifin315@gmail.com>
2024-02-01 21:29:17 +0700

    Add Well-known

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        headers() {
+          return [
+            {
+              source: "/.well-known/apple-app-site-association",
+              headers: [{ key: "content-type", value: "application/json" }]
+            },
+            {
+              source: "/.well-known/assetlinks.json",
+              headers: [{ key: "content-type", value: "application/json" }]
+            }
+          ];
+        }
+      }
+}
 
 module.exports = nextConfig
